1326306eb3dc30c3ebd8e604d5cfd156fd2ac5f5
Prashant Srivastava <prashantsrivastava1994@outlook.com>
2019-12-02 00:21:22 +1100

    First Commit

---
+++ src/client/modules/my/home/home.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class Home extends LightningElement {
+    goToProducts() {
+        this.dispatchEvent(
+            new CustomEvent('navigate', {
+                bubbles: true,
+                composed: true,
+                detail: { path: '/products' }
+            })
+        );
+    }
+}
---
+++ src/client/modules/my/products/products.js
@@ -0,0 +1,43 @@
+import { LightningElement } from 'lwc';
+export default class Products extends LightningElement {
+    productList = [];
+    connectedCallback() {
+        this.productList = [
+            {
+                id: 1,
+                name: 'Car'
+            },
+            {
+                id: 2,
+                name: 'Bike'
+            },
+            {
+                id: 3,
+                name: 'Bus'
+            },
+            {
+                id: 4,
+                name: 'Truck'
+            }
+        ];
+    }
+    viewDetails(e) {
+        let productId = e.target.dataset.productId;
+        this.dispatchEvent(
+            new CustomEvent('navigate', {
+                bubbles: true,
+                composed: true,
+                detail: { path: '/products/' + productId }
+            })
+        );
+    }
+    goBack() {
+        this.dispatchEvent(
+            new CustomEvent('navigate', {
+                bubbles: true,
+                composed: true,
+                detail: { path: '/' }
+            })
+        );
+    }
+}
